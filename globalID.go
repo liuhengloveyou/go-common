35f050ff26c70258b67efcafcf97d4e3229d49a2
liuhengloveyou <liuhengloveyou@icloud.com>
2015-07-03 15:20:03 +0800

    update globalID

--- globalID.go
+++ globalID.go
@@ -7,7 +7,7 @@ import (
 
 type GlobalID struct {
 	servID string
-	monent int64
+	moment string
 	expand string
 	Hole   chan string
 }
@@ -15,14 +15,14 @@ type GlobalID struct {
 func (p *GlobalID) Init(servID, expand string) {
 	p.servID = servID
 	p.expand = expand
-	p.monent = time.Now().Unix()[4:]
+	p.moment = fmt.Sprintf("%d", time.Now().Unix())[4:]
 	p.Hole = make(chan string)
 
 	go func() {
 		var serial int64 = 0
 
 		for {
-			p.Hole <- fmt.Sprintf("%v%v%v%v", p.servID, p.monent, p.expand, serial)
+			p.Hole <- fmt.Sprintf("%v%v%v%v", p.servID, p.moment, p.expand, serial)
 			serial += 1
 		}
 	}()
