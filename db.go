e2e92f48c0ad9b463b354e60b9523bd5febb9963
liuhengloveyou <liuhengloveyou@icloud.com>
2016-01-11 19:20:33 +0800

    add mysql

--- db.go
+++ db.go
@@ -1,11 +1,18 @@
 package common
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/go-xorm/xorm"
+	// "github.com/go-xorm/xorm"
 )
+
+type DBmysql struct {
+	DSN  string
+	conn *sql.DB
+}
 
 type DBConf struct {
 	Name   string `json:"name"`
@@ -14,7 +21,7 @@ type DBConf struct {
 	DSN    string `json:"dsn"`
 }
 
-func InitDBPool(conf interface{}, pool interface{} /* map[string]*xorm.Engine */) (err error) {
+func InitDBPool(conf interface{}, pool interface{}) (err error) {
 	var dbs []DBConf
 	var byteConf []byte
 
@@ -26,8 +33,15 @@ func InitDBPool(conf interface{}, pool interface{} /* map[string]*xorm.Engine */
 	}
 
 	for _, db := range dbs {
-		if db.NGType == "xorm" {
-			pool.(map[string]*xorm.Engine)[db.Name], err = xorm.NewEngine(db.DBType, db.DSN)
+		switch db.NGType {
+		case "xorm":
+			// pool.(map[string]*xorm.Engine)[db.Name], err = xorm.NewEngine(db.DBType, db.DSN)
+		case "mysql":
+			DBConn := &DBmysql{DSN: db.DSN}
+			DBConn.conn, err = sql.Open(db.DBType, db.DSN)
+			pool.(map[string]*DBmysql)[db.Name] = DBConn
+		default:
+			return fmt.Errorf("未知的ng_type: %v", db.NGType)
 		}
 
 		if err != nil {
@@ -36,4 +50,84 @@ func InitDBPool(conf interface{}, pool interface{} /* map[string]*xorm.Engine */
 	}
 
 	return nil
+}
+
+func (this *DBmysql) Query(sqlStr string, args ...interface{}) (rst []map[string]string, err error) {
+	var (
+		stmt *sql.Stmt = nil
+		rows *sql.Rows = nil
+	)
+
+	stmt, err = this.conn.Prepare(sqlStr)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	rows, err = stmt.Query(args...)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	var cols []string
+	cols, err = rows.Columns()
+	if err != nil {
+		return
+	}
+
+	cvals := make([]sql.RawBytes, len(cols))
+	scanArgs := make([]interface{}, len(cols))
+	for i := range cvals {
+		scanArgs[i] = &cvals[i]
+	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return
+		}
+
+		tmap := make(map[string]string, len(cols))
+		for i, col := range cvals {
+			if col == nil {
+				tmap[cols[i]] = ""
+			} else {
+				tmap[cols[i]] = string(col)
+			}
+		}
+		rst = append(rst, tmap)
+	}
+
+	return
+}
+
+func (this *DBmysql) Insert(sqlStr string, args ...interface{}) (int64, error) {
+	stmt, err := this.conn.Prepare(sqlStr)
+	if err != nil {
+		return -1, err
+	}
+	defer stmt.Close()
+
+	rst, err := stmt.Exec(args...)
+	if err != nil {
+		return -1, err
+	}
+
+	return rst.LastInsertId()
+}
+
+func (this *DBmysql) Update(sqlStr string, args ...interface{}) (int64, error) {
+	stmt, err := this.conn.Prepare(sqlStr)
+	if err != nil {
+		return -1, err
+	}
+	defer stmt.Close()
+
+	rst, err := stmt.Exec(args...)
+	if err != nil {
+		return -1, err
+	}
+
+	return rst.RowsAffected()
 }
